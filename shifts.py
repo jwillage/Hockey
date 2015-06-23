68059157045d7c3be12af61288a300cf4500e919
jwillage <jwillage@gmail.com>
2015-06-23 19:08:23 -0400

    added home shifts and write both to 1 file

--- shifts.py
+++ shifts.py
@@ -9,53 +9,98 @@ gameId = sys.argv[2]
 
 awayPage = requests.get('http://www.nhl.com/scores/htmlreports/' + season + str(int(season) + 1) + '/TV0' + gameId + '.HTM')
 # awayPage = requests.get('http://www.nhl.com/scores/htmlreports/20142015/TV030227.HTM')
+homePage = requests.get('http://www.nhl.com/scores/htmlreports/' + season + str(int(season) + 1) + '/TH0' + gameId + '.HTM')
+# homePage = requests.get('http://www.nhl.com/scores/htmlreports/20142015/TH030227.HTM')
 awayTree = html.fromstring(awayPage.text)
 awayPlayers = awayTree.xpath('//table/tr[position()=4]/td/table/tr/td[@class="playerHeading + border"]/text()')
+homeTree = html.fromstring(homePage.text)
+homePlayers = homeTree.xpath('//table/tr[position()=4]/td/table/tr/td[@class="playerHeading + border"]/text()')
+
 
 #get period, shift start, shift duration
 awayShifts = awayTree.xpath('//table[position() = 1]/tr[position()=4]/td/table/tr[contains(@class, "Color")]/td[position() = 2 or position() = 3 or position() = 5]/text()')
+homeShifts = homeTree.xpath('//table[position() = 1]/tr[position()=4]/td/table/tr[contains(@class, "Color")]/td[position() = 2 or position() = 3 or position() = 5]/text()')
 
 #get the total number of shifts for each player
-shiftCount = awayTree.xpath('//td[@colspan="8"]/table/tr[position() = last()]/td[position() = 2]/text()')
+awayShiftCount = awayTree.xpath('//td[@colspan="8"]/table/tr[position() = last()]/td[position() = 2]/text()')
+homeShiftCount = homeTree.xpath('//td[@colspan="8"]/table/tr[position() = last()]/td[position() = 2]/text()')
 
-x = []
+awayTokens = []
+homeTokens = []
 
 cum = 0
-temp = []
 for i in range(0, len(awayPlayers)):
-	temp = []
-	#append player name 
-	temp.append(awayPlayers[i][awayPlayers[i].index(',') + 2 : ] + awayPlayers[i][awayPlayers[i].index(' ') : awayPlayers[i].index(',') ])
-	temp.append(awayShifts[cum * 3 : (cum + int(shiftCount[i])) * 3])
-	cum += int(shiftCount[i])
-	x.append(temp)
+	temp = awayShifts[cum * 3 : (cum + int(awayShiftCount[i])) * 3]
+	cum += int(awayShiftCount[i])
+	awayTokens.append(temp)
 
-ret = []
+cum = 0
+for i in range(0, len(homePlayers)):
+	temp = homeShifts[cum * 3 : (cum + int(homeShiftCount[i])) * 3]
+	cum += int(homeShiftCount[i])
+	homeTokens.append(temp)
+
+awayRet = []
+
+#shift start time is exclusive ie players are on the ice at 0. if a player is also on-ice at 2, that is the third second (0, 1, 2)
+#but you can also be on the ice for the end of period (20:00)
 for i in range(0, len(awayPlayers)):
-	temp = ['F'] * 4800 #TODO change 3600 to actual seconds in game (use GEND from pbp page, or see if its game info page, etc)
-	for j in range(0, len(x[i][1]) - 1, 3):
-		shiftStart = (((int(x[i][1][j]) - 1) * 1200) + int(x[i][1][j + 1][ : x[i][1][j + 1].index(':')]) * 60) + int(x[i][1][j + 1][x[i][1][j + 1].index(':') + 1 : x[i][1][j + 1].index(' ')])
+	temp = ['F'] * 4805 #TODO change 3600 to actual seconds in game (use GEND from pbp page, or see if its game info page, etc)
+	#4 periods = 4800 + 1 for player name + 4 for each period start at 0:00
+	#append player name 
+	temp[0] = awayPlayers[i][awayPlayers[i].index(',') + 2 : ] + awayPlayers[i][awayPlayers[i].index(' ') : awayPlayers[i].index(',') ]
+	for j in range(0, len(awayTokens[i][0]) - 1, 3):
+		shiftStart = (((int(awayTokens[i][0][j]) - 1) * 1200) + int(awayTokens[i][0][j + 1][ : awayTokens[i][0][j + 1].index(':')]) * 60) + int(awayTokens[i][0][j + 1][awayTokens[i][0][j + 1].index(':') + 1 : awayTokens[i][0][j + 1].index(' ')])
 #									#period														#shift start min												turn min to sec						seconds portion of shift start
-		shiftLength = (int(x[i][1][j + 2][ : x[i][1][j + 2].index(':')]) * 60) + int(x[i][1][j + 2][x[i][1][j + 2].index(':') + 1 : ])
+		shiftLength = (int(awayTokens[i][0][j + 2][ : awayTokens[i][0][j + 2].index(':')]) * 60) + int(awayTokens[i][0][j + 2][awayTokens[i][0][j + 2].index(':') + 1 : ])
 #											#minutes of shift length													#seconds portion of shift length
 		for k in range(shiftStart, shiftStart + shiftLength):
-			temp[k - 1] = 'T'
-	ret.append(awayPlayers[i])
-	ret.append(temp)
+			#if the name is not appended at the start, change to temp[k-1]
+			temp[k + 1] = 'T'
+	awayRet.append(temp)
 
-nisky through 1240 secs T is working
-2nd period, 40 seconds elapsed
+homeRet = []
 
-f = open("/mnt/hgfs/VM/" + season + "_" + gameId + "_pbp", 'w')
+#shift start time is exclusive ie players are on the ice at 0. if a player is also on-ice at 2, that is the third second (0, 1, 2)
+#but you can also be on the ice for the end of period (20:00)
+for i in range(0, len(homePlayers)):
+	temp = ['F'] * 4805 #TODO change 3600 to actual seconds in game (use GEND from pbp page, or see if its game info page, etc)
+	#4 periods = 4800 + 1 for player name + 4 for each period start at 0:00
+	#append player name 
+	temp[0] = homePlayers[i][homePlayers[i].index(',') + 2 : ] + homePlayers[i][homePlayers[i].index(' ') : homePlayers[i].index(',') ]
+	for j in range(0, len(homeTokens[i][0]) - 1, 3):
+		shiftStart = (((int(homeTokens[i][0][j]) - 1) * 1200) + int(homeTokens[i][0][j + 1][ : homeTokens[i][0][j + 1].index(':')]) * 60) + int(homeTokens[i][0][j + 1][homeTokens[i][0][j + 1].index(':') + 1 : homeTokens[i][0][j + 1].index(' ')])
+#									#period														#shift start min												turn min to sec						seconds portion of shift start
+		shiftLength = (int(homeTokens[i][0][j + 2][ : homeTokens[i][0][j + 2].index(':')]) * 60) + int(homeTokens[i][0][j + 2][homeTokens[i][0][j + 2].index(':') + 1 : ])
+#											#minutes of shift length													#seconds portion of shift length
+		for k in range(shiftStart, shiftStart + shiftLength):
+			#if the name is not appended at the start, change to temp[k-1]
+			temp[k + 1] = 'T'
+	homeRet.append(temp)
+
+
+f = open("/mnt/hgfs/VM/" + season + "_" + gameId + "_shifts", 'w')
+
 cnt = 0
-f.write('season,gameId,id,per,strength,elapsed,remaining,event,primaryTeam,primaryPlayer,secondaryTeam,secondaryPlayer,zone,shotType,distance,miss,stop,penType,penTime,firstAssist,secondAssist\r\n')
-
-while cnt < len(plays):
+while cnt < len(awayRet):
 	fieldCnt = 0
-	f.write(season + ', ' + gameId + ', ')
-	while fieldCnt < len(plays[cnt]):
-		f.write(plays[cnt][fieldCnt].encode('utf-8'))
-		if fieldCnt != len(plays[cnt])- 1:
+	f.write(season + ', ' + gameId + ', away, ' )
+	while fieldCnt < len(awayRet[cnt]):
+		f.write(awayRet[cnt][fieldCnt])
+		if fieldCnt != len(awayRet[cnt])- 1:
+			f.write(', ')
+		else:
+			f.write('\r\n')
+		fieldCnt += 1
+	cnt += 1
+
+cnt = 0
+while cnt < len(homeRet):
+	fieldCnt = 0
+	f.write(season + ', ' + gameId + ', home, ')
+	while fieldCnt < len(homeRet[cnt]):
+		f.write(homeRet[cnt][fieldCnt])
+		if fieldCnt != len(homeRet[cnt])- 1:
 			f.write(', ')
 		else:
 			f.write('\r\n')
