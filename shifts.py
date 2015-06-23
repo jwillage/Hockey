370bf2378711b961ef820fe0f09e99998e5ae1f7
jwillage <jwillage@gmail.com>
2015-06-23 18:08:02 -0400

    fixed total elapsed data. Now adds 20 minutes per period vs 20 seconds

--- shifts.py
+++ shifts.py
@@ -10,13 +10,41 @@ gameId = sys.argv[2]
 awayPage = requests.get('http://www.nhl.com/scores/htmlreports/' + season + str(int(season) + 1) + '/TV0' + gameId + '.HTM')
 # awayPage = requests.get('http://www.nhl.com/scores/htmlreports/20142015/TV030227.HTM')
 awayTree = html.fromstring(awayPage.text)
-awayPlayerTokens = awayTree.xpath('//table/tr[position()=4]/td/table/tr/td[@class="playerHeading + border"]/text()')
+awayPlayers = awayTree.xpath('//table/tr[position()=4]/td/table/tr/td[@class="playerHeading + border"]/text()')
 
+#get period, shift start, shift duration
+awayShifts = awayTree.xpath('//table[position() = 1]/tr[position()=4]/td/table/tr[contains(@class, "Color")]/td[position() = 2 or position() = 3 or position() = 5]/text()')
 
+#get the total number of shifts for each player
+shiftCount = awayTree.xpath('//td[@colspan="8"]/table/tr[position() = last()]/td[position() = 2]/text()')
 
-plays = []
-temp=[]
+x = []
 
+cum = 0
+temp = []
+for i in range(0, len(awayPlayers)):
+	temp = []
+	#append player name 
+	temp.append(awayPlayers[i][awayPlayers[i].index(',') + 2 : ] + awayPlayers[i][awayPlayers[i].index(' ') : awayPlayers[i].index(',') ])
+	temp.append(awayShifts[cum * 3 : (cum + int(shiftCount[i])) * 3])
+	cum += int(shiftCount[i])
+	x.append(temp)
+
+ret = []
+for i in range(0, len(awayPlayers)):
+	temp = ['F'] * 4800 #TODO change 3600 to actual seconds in game (use GEND from pbp page, or see if its game info page, etc)
+	for j in range(0, len(x[i][1]) - 1, 3):
+		shiftStart = (((int(x[i][1][j]) - 1) * 1200) + int(x[i][1][j + 1][ : x[i][1][j + 1].index(':')]) * 60) + int(x[i][1][j + 1][x[i][1][j + 1].index(':') + 1 : x[i][1][j + 1].index(' ')])
+#									#period														#shift start min												turn min to sec						seconds portion of shift start
+		shiftLength = (int(x[i][1][j + 2][ : x[i][1][j + 2].index(':')]) * 60) + int(x[i][1][j + 2][x[i][1][j + 2].index(':') + 1 : ])
+#											#minutes of shift length													#seconds portion of shift length
+		for k in range(shiftStart, shiftStart + shiftLength):
+			temp[k - 1] = 'T'
+	ret.append(awayPlayers[i])
+	ret.append(temp)
+
+nisky through 1240 secs T is working
+2nd period, 40 seconds elapsed
 
 f = open("/mnt/hgfs/VM/" + season + "_" + gameId + "_pbp", 'w')
 cnt = 0
